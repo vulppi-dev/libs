7d0ef1b7bc994ea9c0fef4d994aea8567dd7f627
Renato Rodrigues <morbden.dev@gmail.com>
2023-02-14 11:01:29 -0300

    novos tipos

--- libs/toolbelt/src/index.ts
+++ libs/toolbelt/src/index.ts
@@ -6,11 +6,20 @@ export declare type Null = null | undefined
 
 export declare type Nullable<T = any> = Null | T
 
+/**
+ * Generic type for html elements
+ */
 export declare type NodeElement = Element | HTMLElement | SVGElement
 
+/**
+ * Accepts generic strings and known string keys
+ */
 export declare type Unstring<K extends string> = K | Omit<string, K>
 
-export declare interface Directive<
+/**
+ * Svelte directive typing
+ */
+export declare interface SvelteDirective<
   P extends any = any,
   E extends NodeElement = NodeElement,
 > {
@@ -23,12 +32,27 @@ export declare interface Directive<
     | void
 }
 
+/**
+ * @deprecated
+ * Use SvelteDirective instead
+ */
+export declare type Directive<
+  P extends any = any,
+  E extends NodeElement = NodeElement,
+> = SvelteDirective<P, E>
+
 export declare type VoidFunction = () => void
 
 export declare type UnsubscribeList = (() => void)[]
 
+/**
+ * Union of values of an array
+ */
 export declare type ValuesOf<T extends readonly any[]> = T[number]
 
+/**
+ * Array of function parameters
+ */
 export declare type Args<F extends Function> = F extends (
   ...args: infer A
 ) => any
@@ -38,3 +62,42 @@ export declare type Args<F extends Function> = F extends (
 export declare type Arguments<F extends Function> = Args<F>
 
 export declare type Params<F extends Function> = Args<F>
+
+/**
+ * Make all properties in T optional (deep)
+ * @example
+ * type T0 = {
+ *   a: {
+ *     b: {
+ *       c: number
+ *     }
+ *   }
+ * }
+ * type T1 = PartialDeep<T0> // { a?: { b?: { c?: number } } }
+ */
+export declare type PartialDeep<T> = {
+  [P in keyof T]?: T[P] extends object ? PartialDeep<T[P]> : T[P]
+}
+
+/**
+ * Make all properties in T required (deep)
+ * @example
+ * type T0 = {
+ *   a?: {
+ *     b?: {
+ *       c?: number
+ *     }
+ *   }
+ * }
+ * type T1 = RequiredDeep<T0> // { a: { b: { c: number } } }
+ */
+export declare type RequiredDeep<T> = {
+  [P in keyof T]-?: T[P] extends object ? RequiredDeep<T[P]> : T[P]
+}
+
+/**
+ * Make all properties in T readonly (deep)
+ */
+export declare type ReadonlyDeep<T> = {
+  readonly [P in keyof T]: T[P] extends object ? ReadonlyDeep<T[P]> : T[P]
+}
