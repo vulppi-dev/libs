da388917518b54286c95bec8e8bfd6ebe1c5de9a
Renato Rodrigues <morbden.dev@gmail.com>
2023-06-12 08:31:43 -0300

    fix toolbelt type serialize

--- libs/toolbelt/src/serialize.ts
+++ libs/toolbelt/src/serialize.ts
@@ -1,10 +1,9 @@
-import { Buffer } from 'buffer'
 import { deflate, inflate } from 'pako'
 
 export const serializeObject = (obj: any) => {
   return deflate(JSON.stringify(obj))
 }
 
-export const deserializeObject = <R = any>(serial: Buffer): R => {
+export const deserializeObject = <R = any>(serial: ArrayBuffer): R => {
   return JSON.parse(inflate(serial, { to: 'string' }).toString())
 }
