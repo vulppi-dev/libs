4b323073172fc6a8c8392d90cfde8068035d4135
Renato Rodrigues <renato@vulppi.dev>
2025-03-23 11:09:34 -0300

    feat: update package version and add tryCatch utility functions for error handling

--- toolbelt/src/functions.ts
+++ toolbelt/src/functions.ts
@@ -75,3 +75,78 @@ export function omitNullables<R extends object>(obj: R): R {
   }
   return result as R
 }
+
+/**
+ * The tryCatch function is a utility function that allows you to run an async function
+ * and catch any errors that occur without a block try/catch statement.
+ *
+ * @param cb - Function to run
+ * @returns [result, error]
+ *
+ * @example
+ * const [result, error] = await tryCatch(async () => {
+ *   const data = await fetchData();
+ *   return data;
+ * });
+ * if (error) {
+ *   console.error('Error fetching data:', error);
+ * } else {
+ *   console.log('Data:', result);
+ * }
+ */
+export async function tryCatch<
+  F extends (...args: any[]) => any | Promise<any>,
+>(cb: F): Promise<[ReturnType<F>, null] | [null, Error]> {
+  try {
+    return [await cb(), null] as [ReturnType<F>, null]
+  } catch (err: any) {
+    if (err instanceof Error) {
+      return [null, err]
+    } else if (typeof err === 'string') {
+      return [null, new Error(err)]
+    } else {
+      return [
+        null,
+        new Error('Unknown error', {
+          cause: err,
+        }),
+      ]
+    }
+  }
+}
+
+/**
+ * The tryCatchPromise function is a utility function that allows you
+ * catch any errors that occur without a block try/catch statement or then/catch methods.
+ *
+ * @param promise - Promise to await
+ * @returns [result, error]
+ *
+ * @example
+ * const [result, error] = await tryCatchPromise(fetchData());
+ * if (error) {
+ *   console.error('Error fetching data:', error);
+ * } else {
+ *   console.log('Data:', result);
+ * }
+ */
+export async function tryCatchPromise<T>(
+  promise: Promise<T>,
+): Promise<[T, null] | [null, Error]> {
+  try {
+    return [await promise, null]
+  } catch (err: any) {
+    if (err instanceof Error) {
+      return [null, err]
+    } else if (typeof err === 'string') {
+      return [null, new Error(err)]
+    } else {
+      return [
+        null,
+        new Error('Unknown error', {
+          cause: err,
+        }),
+      ]
+    }
+  }
+}
